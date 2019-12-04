5f9438f8efba564fad1313011c04217ecd2c9a83
juljasong <juljasong@gmail.com>
2019-12-04 18:07:17 +0900

    1204

---
+++ bitcamp-java/src/main/java/com/ohoracs/basic/Hello3.java
@@ -0,0 +1,9 @@
+package com.ohoracs.basic;
+
+public class Hello3 {
+
+  public static void main(String[] args) {
+
+    System.out.println("basic/HELLO3");
+  }
+}
---
+++ bitcamp-java/src/main/java/com/ohoracs/ok/Hello3.java
@@ -0,0 +1,9 @@
+package com.ohoracs.ok;
+
+public class Hello3 {
+
+  public static void main(String[] args) {
+
+    System.out.println("ok/HELLO3");
+  }
+}
