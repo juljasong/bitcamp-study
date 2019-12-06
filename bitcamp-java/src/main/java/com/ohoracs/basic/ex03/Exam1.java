f759df4a5a276a322c14b7534d93b8f83d442216
juljasong <juljasong@gmail.com>
2019-12-06 17:20:01 +0900

    1206

--- bitcamp-java/src/main/java/com/ohoracs/basic/ex03/Exam1.java
+++ bitcamp-java/src/main/java/com/ohoracs/basic/ex03/Exam1.java
@@ -19,10 +19,23 @@ public class Exam1 {
     System.out.println(-0b01001110); //2진수
 
     //부동소수점 리터럴
+    
+    System.out.println(3.14); // 고정소수점
+    System.out.println(0.314e1);  //10의 1승
+    System.out.println(31.4e-1);
 
     //논리 리터럴
-    
 
+    System.out.println(true);
+    System.out.println(false);
+    
     //문자 리터럴
+
+    System.out.println('A');
+    System.out.println('가');
+    
+    //문자열 리터럴
+    
+    System.out.println("안녕하세요 감사해요 잘 있어요");
   }
 }
