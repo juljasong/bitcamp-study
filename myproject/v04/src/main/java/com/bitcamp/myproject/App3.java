d721790b9b8eaed48566b1934290f978ef602bcb
juljasong <juljasong@gmail.com>
2019-12-10 17:30:37 +0900

    1210

---
+++ myproject/v04/src/main/java/com/bitcamp/myproject/App3.java
@@ -0,0 +1,17 @@
+package com.bitcamp.myproject;
+
+public class App3 {
+
+  public static void main(String[] args) {
+    int bbsNum = 1;
+    String bbsName = "제목입니다.";
+    String bbsDate = "2019-12-10";
+    int bbsHits = 0;
+
+    System.out.println("게시글 번호 : " + bbsNum);
+    System.out.println("제목 : " + bbsName);
+    System.out.println("작성일자 : " + bbsDate);
+    System.out.println("조회수 : " + bbsHits);
+  }
+
+}
