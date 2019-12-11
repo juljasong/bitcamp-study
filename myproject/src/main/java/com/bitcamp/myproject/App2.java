af804e519c5cb64a1cea82d58504ec138106e0d3
juljasong <juljasong@gmail.com>
2019-12-11 10:26:46 +0900

    v04

--- myproject/src/main/java/com/bitcamp/myproject/App2.java
+++ myproject/src/main/java/com/bitcamp/myproject/App2.java
@@ -4,17 +4,15 @@ public class App2 {
 
     public static void main(String[] args) {
       int userNum = 1;
-      String userName = "송율리아";
-      String userID = "julia";
-      String userPW = "julia00";
       String userEmail = "julia@gmail.com";
+      String userPW = "julia00";
+      String userName = "송율리아";
       String userDate = "2019-12-10";
       
       System.out.println("회원번호 : " + userNum);
-      System.out.println("회원명 : " + userName);
-      System.out.println("회원ID : " + userID);
-      System.out.println("비밀번호 : " + userPW);
       System.out.println("이메일 : " + userEmail);
+      System.out.println("비밀번호 : " + userPW);
+      System.out.println("회원명 : " + userName);
       System.out.println("가입일 : " + userDate);
     }
 }
