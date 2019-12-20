cb6da23534a5109db23dde53bbf162112e5b9d39
juljasong <juljasong@gmail.com>
2019-12-20 11:53:52 +0900

    1220

---
+++ bitcamp-project/v08/src/main/java/com/eomcs/lms/Board.java
@@ -0,0 +1,9 @@
+package com.eomcs.lms;
+
+import java.sql.Date;
+
+public class Board {
+  int no, viewCount;
+  String title;
+  Date date;
+}
---
+++ bitcamp-project/v08/src/main/java/com/eomcs/lms/User.java
@@ -0,0 +1,11 @@
+package com.eomcs.lms;
+
+import java.sql.Date;
+
+public class User {
+  
+  int no;
+  String name, email, password, photo, tel;
+  Date registeredDate;
+
+}
