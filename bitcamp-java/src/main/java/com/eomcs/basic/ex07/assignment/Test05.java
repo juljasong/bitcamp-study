6ab3d239d46f77b54387f09a23d0a39e829dfeb5
juljasong <juljasong@gmail.com>
2019-12-23 18:29:52 +0900

    1223

---
+++ bitcamp-java/src/main/java/com/eomcs/basic/ex07/assignment/Test05.java
@@ -0,0 +1,38 @@
+package com.eomcs.basic.ex07.assignment;
+
+import java.util.Scanner;
+
+// 과제: 재귀호출을 이용하여 직삼각형을 출력하라.
+// 실행)
+// 밑변의 길이? 5
+// *
+// **
+// ***
+// ****
+// *****
+
+public class Test05 {
+  
+  static int base;
+  
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.print("밑변의 길이? "); // 사용자로부터 밑변의 길이를 입력 받는다.
+    base = sc.nextInt();
+    sc.close();
+    
+    printTriangle(base); // 직삼각형을 출력한다.
+  }
+
+  static void printTriangle(int base1) {
+    if (base1 == 0) {
+      
+    } else {
+      for (int i = 0 ; i <= base-base1 ; i++)
+      System.out.print("*");
+      System.out.println();
+      printTriangle(base1 - 1);
+    }
+  }
+}
