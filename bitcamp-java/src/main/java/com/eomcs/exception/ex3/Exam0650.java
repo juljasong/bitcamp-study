fc04a252ab405fffa28d6fb2afae7078b61db3ee
juljasong <juljasong@gmail.com>
2020-01-28 00:40:25 +0900

    0127

--- bitcamp-java/src/main/java/com/eomcs/exception/ex3/Exam0650.java
+++ bitcamp-java/src/main/java/com/eomcs/exception/ex3/Exam0650.java
@@ -3,29 +3,24 @@ package com.eomcs.exception.ex3;
 
 public class Exam0650 {
 
-  static class A {}
+  static class A {
+  }
 
   static class B implements AutoCloseable {
+    @Override
     public void close() throws Exception {
       System.out.println("B 클래스의 자원을 해제하였습니다.");
     }
   }
 
-  static void m() throws Exception {
+  public static void main(String[] args) throws Exception {
+    B obj2 = null;
 
     try (
-        // A 클래스는 AutoCloseable 구현체가 아니기 때문에 여기에 선언할 수 없다.
-        //A obj = new A(); // 컴파일 오류!
-
-        B obj2 = new B(); // OK!
-        ) {
+        obj2 = new B(); // 컴파일 오류!
+        // 변수 선언은 반드시 괄호 안에 해야 한다.
+    ) {
       System.out.println("try 블록 실행...");
     }
-    // finally 블록에서 B의 close()를 호출하지 않아도,
-    // 자동으로 호출될 것이다. 실행하여 확인하라!
   }
-  public static void main(String[] args) throws Exception {
-    m();
-  }
-
 }
--- bitcamp-java/src/main/java/com/eomcs/ok/Hello3.java
+++
@@ -1,9 +0,0 @@
-package com.eomcs.ok;
-
-public class Hello3 {
-
-  public static void main(String[] args) {
-
-    System.out.println("ok/HELLO3");
-  }
-}
