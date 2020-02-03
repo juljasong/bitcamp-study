5b803ab216f15dd12405c9842124839eca1cd6e6
juljasong <juljasong@gmail.com>
2020-02-03 17:08:47 +0900

    0203

--- bitcamp-project-server/src/test/java/com/eomcs/lms/ServerAppTest.java
+++ bitcamp-project-server/src/test/java/com/eomcs/lms/ServerAppTest.java
@@ -3,12 +3,6 @@
  */
 package com.eomcs.lms;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-
 public class ServerAppTest {
-    @Test public void testAppHasAGreeting() {
-        ServerApp classUnderTest = new ServerApp();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-    }
+
 }
