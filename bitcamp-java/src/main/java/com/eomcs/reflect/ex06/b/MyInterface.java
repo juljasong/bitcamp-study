8843205bb15e7ca9bd3c878e9f4a24317cdc702a
juljasong <juljasong@gmail.com>
2020-03-12 17:16:21 +0900

    0312

---
+++ bitcamp-java/src/main/java/com/eomcs/reflect/ex06/b/MyInterface.java
@@ -0,0 +1,9 @@
+package com.eomcs.reflect.ex06.b;
+
+public interface MyInterface {
+
+  void m1();
+
+  void m2(int v1, int v2);
+
+}
---
+++ bitcamp-java/src/main/java/com/eomcs/reflect/ex06/d/MyInterface.java
@@ -0,0 +1,9 @@
+package com.eomcs.reflect.ex06.d;
+
+public interface MyInterface {
+
+  int m1(int v1, int v2);
+
+  String m2(String name, int v2);
+
+}
