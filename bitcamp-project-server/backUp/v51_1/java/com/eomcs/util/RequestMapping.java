104b1f85a24eb1be182a752ec4b8a1692d4d1395
juljasong <juljasong@gmail.com>
2020-03-18 12:58:20 +0900

    v51_1

---
+++ bitcamp-project-server/backUp/v51_1/java/com/eomcs/util/RequestMapping.java
@@ -0,0 +1,14 @@
+package com.eomcs.util;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME) // 실행 중에 메서드에 붙은 어노테이션 정보 추출
+@Target({ElementType.METHOD})
+public @interface RequestMapping {
+
+  String value();
+
+}
