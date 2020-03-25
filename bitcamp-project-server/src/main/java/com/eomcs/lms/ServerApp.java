4140f7565fb795bf7aadf10de2baa62c04f1ecb6
juljasong <juljasong@gmail.com>
2020-03-25 17:49:21 +0900

    v54_2

--- bitcamp-project-server/src/main/java/com/eomcs/lms/ServerApp.java
+++ bitcamp-project-server/src/main/java/com/eomcs/lms/ServerApp.java
@@ -7,6 +7,7 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URLDecoder;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -119,20 +120,26 @@ public class ServerApp {
       ServerApp.logger.info(String.format("method => %s", method));
       ServerApp.logger.info(String.format("request-uri => %s", requestUri));
 
+      String servletPath = getServletPath(requestUri);
+
+      ServerApp.logger.debug(String.format("resource url => %s", servletPath));
+
+      Map<String, String> params = getParametersFromQueryString(requestUri);
+
       // HTTP 응답 헤더 출력
       printResponseHeader(out);
 
-      if (requestUri.equalsIgnoreCase("/server/stop")) {
+      if (servletPath.equalsIgnoreCase("/server/stop")) {
         quit(out);
         return;
       }
 
-      RequestHandler requestHandler = handlerMapper.getHandler(requestUri);
+      RequestHandler requestHandler = handlerMapper.getHandler(servletPath);
 
       if (requestHandler != null) {
         try {
-          // servlet.service(in, out);
-          requestHandler.getMethod().invoke(requestHandler.getBean(), in, out);
+          // request handler 메서드 호출
+          requestHandler.getMethod().invoke(requestHandler.getBean(), params, out);
         } catch (Exception e) {
           out.println("요청 처리 중 오류 발생!");
           out.println(e.getMessage());
@@ -147,7 +154,6 @@ public class ServerApp {
         notFound(out);
         ServerApp.logger.info("해당 명령을 지원하지 않습니다.");
       }
-      out.println("!end!");
       out.flush();
       ServerApp.logger.info("클라이언트에게 응답하였음!");
 
@@ -160,20 +166,51 @@ public class ServerApp {
   }
 
   private void notFound(PrintStream out) throws IOException {
-    out.println("요청한 명령을 처리할 수 없습니다.");
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<meta charset='UTF-8'>");
+    out.println("<title>실행 오류</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>실행 오류</h1>");
+    out.println("<p>요청한 명령을 처리할 수 없습니다.</p>");
   }
 
   private void quit(PrintStream out) throws IOException {
     serverStop = true;
-    out.println("OK");
-    out.println("!end!");
     out.flush();
   }
 
   private void printResponseHeader(PrintStream out) {
     out.println("HTTP/1.1 200OK");
-    out.println("Server: Apache");
+    out.println("Server: bitcampServer");
     out.println();
+  }
+
+  private String getServletPath(String requestUri) {
+    // requestUri => /member/add?email=aaa@test.com&name=1111&password=1234
+    return requestUri.split("\\?")[0]; // ex) /member/add
+  }
+
+  private Map<String, String> getParametersFromQueryString(String requestUri) throws Exception {
+    String[] items = requestUri.split("\\?");
+    // 데이터(Query String) 따로 저장
+    // => email=aaa@test.com&name=aaa&password=1111
+    Map<String, String> params = new HashMap<>();
+    if (items.length > 1) {
+      ServerApp.logger.debug(String.format("query string => %s", items[1])); // info:시스템 운영자
+                                                                             // debug:개발자
+      String[] entries = items[1].split("&");
+      for (String entry : entries) {
+        ServerApp.logger.debug(String.format("parameter => %s", entry));
+        String[] kv = entry.split("=");
+        // 웹브라우저가 URL 인코딩하여 보낸 데이터 디코딩 필요
+        String value = URLDecoder.decode(kv[1], "UTF-8");
+        params.put(kv[0], value);
+      }
+    }
+    return params;
   }
 
   public static void main(String[] args) {
