98751ede0f6ebb00d9309795f25f37c2b0fb4c69
juljasong <juljasong@gmail.com>
2020-03-25 13:01:31 +0900

    v54_1 : HTTP Protocol

---
+++ bitcamp-project-server/backUp/v54_1/java/com/eomcs/lms/dao/LessonDao.java
@@ -0,0 +1,25 @@
+package com.eomcs.lms.dao;
+
+import java.util.List;
+import java.util.Map;
+import com.eomcs.lms.domain.Lesson;
+
+public interface LessonDao {
+
+  // 서블릿 객체들이 데이터를 다룰 때 사용할 메서드를 정의한다.
+  int insert(Lesson lesson) throws Exception;
+
+  List<Lesson> findAll() throws Exception;
+
+  Lesson findByNo(int no) throws Exception;
+
+  int update(Lesson lesson) throws Exception;
+
+  int delete(int no) throws Exception;
+
+  default List<Lesson> findByKeyword(Map<String, Object> params) throws Exception {
+    return null;
+  }
+}
+
+
